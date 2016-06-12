07ef7391ad06951a0aa09bf82cfd8fe1f664e547
Tavorc <tavorcoh@gmail.com>
2016-06-13 00:07:15 +0300

    youtube

--- app/src/main/java/il/ac/shenkar/showshenkar/activities/AudioActivity.java
+++ app/src/main/java/il/ac/shenkar/showshenkar/activities/AudioActivity.java
@@ -96,18 +96,4 @@ public class AudioActivity extends AppCompatActivity {
             myHandler.postDelayed(this, 100);
         }
     };
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
