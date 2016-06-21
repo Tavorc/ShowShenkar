09cf97af6690a1ccf4426f7e5c91b7c0c760128c
SagiMaday <sagimaday@gmail.com>
2016-06-21 15:49:30 +0300

    QR code added in "ShenkarActivity" *OpenScanner *onActivityResult 2 adapters added for the QR *IntentIntegrator *IntentResult

--- app/src/main/java/il/ac/shenkar/showshenkar/activities/ShenkarActivity.java
+++ app/src/main/java/il/ac/shenkar/showshenkar/activities/ShenkarActivity.java
@@ -1,15 +1,20 @@
 package il.ac.shenkar.showshenkar.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import il.ac.shenkar.showshenkar.R;
+import il.ac.shenkar.showshenkar.adapters.IntentIntegrator;
+import il.ac.shenkar.showshenkar.adapters.IntentResult;
 
 public class ShenkarActivity extends AppCompatActivity {
 
+    public String Qrlocation;
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu items for use in the action bar
@@ -23,10 +28,27 @@ public class ShenkarActivity extends AppCompatActivity {
         int id = item.getItemId();
         if (id == R.id.action_camera)
         {
-            // TODO: implement camera QR logic
-            Toast.makeText(this, R.string.camera_btn, Toast.LENGTH_LONG).show();
+            OpenScanner();
         }
-
         return true;
+    }
+
+    public void OpenScanner()
+    {
+        IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+        scanIntegrator.initiateScan();
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+            String scanContent = scanningResult.getContents();
+            //String scanFormat = scanningResult.getFormatName();
+            Qrlocation = scanContent;
+        }
+        else{
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
