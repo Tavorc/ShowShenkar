4b1367b3cfc72640251a6316a7f7e8bb893f297f
TomGoldberg <tomg3210@gmail.com>
2016-06-28 22:33:57 +0300

    Google Maps : 2 ground overlay images definition, 2 new images in .mipmap folder, new java class PermissionUtils under .utils folder

--- app/src/main/java/il/ac/shenkar/showshenkar/activities/MapActivity.java
+++ app/src/main/java/il/ac/shenkar/showshenkar/activities/MapActivity.java
@@ -1,55 +1,93 @@
 package il.ac.shenkar.showshenkar.activities;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.LocationManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import il.ac.shenkar.showshenkar.R;
-import il.ac.shenkar.showshenkar.utils.CurrentLocationListener;
+import il.ac.shenkar.showshenkar.utils.PermissionUtils;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapActivity extends FragmentActivity implements ActivityCompat.OnRequestPermissionsResultCallback,OnMapReadyCallback,GoogleMap.OnMyLocationButtonClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap depatmentMap;
-    private final List<BitmapDescriptor> mImages = new ArrayList<BitmapDescriptor>();
-    GroundOverlay mGroundOverlay;
-    private GroundOverlay mGroundOverlayRotated;
-    LocationManager locationManager;
 
+    private GoogleApiClient mGoogleApiClient;
+
+    private Location mLastLocation;
+
+    private static final int TRANSPARENCY_MAX = 100;
+
+    private static final LatLng SHENKAR = new LatLng(32.09039421212218, 34.8030037432909);
+    private static final LatLng ELIT = new LatLng(32.08264557800064, 34.80440218001604);
+
+    private static final LatLng SHENKAR_OVERVIEW = new LatLng(SHENKAR.latitude - 0.001, SHENKAR.longitude - 0.001);
+
+    private final List<BitmapDescriptor> mImages = new ArrayList<BitmapDescriptor>();
+
+    private GroundOverlay mGroundOverlay;
+    private GroundOverlay mGroundOverlay2;
+
+    private GroundOverlay mGroundOverlayRotated;
+
+    private SeekBar mTransparencyBar;
+
+    private int mCurrentEntry = 0;
+
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean mPermissionDenied = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.department_map);
         mapFragment.getMapAsync(this);
 
-        //get Your Current Location
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        CurrentLocationListener locationListener = new CurrentLocationListener();
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
         }
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
     }
 
@@ -57,22 +95,126 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         depatmentMap = googleMap;
 
-        // Add a marker in Shenkar and move the camera
-        LatLng Shenkar = new LatLng(-32, 121);
-        depatmentMap.addMarker(new MarkerOptions().position(Shenkar).title("Shenkar"));
-        depatmentMap.moveCamera(CameraUpdateFactory.newLatLng(Shenkar));
-        //depatmentMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Shenkar, 4));
+        depatmentMap.setOnMyLocationButtonClickListener(this);
+        enableMyLocation();
 
-        //mImages.clear();
-        //mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.building));
+        // Register a listener to respond to clicks on GroundOverlays.
+        //depatmentMap.setOnGroundOverlayClickListener(this);
+
+        depatmentMap.moveCamera(CameraUpdateFactory.newLatLngZoom(  SHENKAR  , 18  ));
+
+        mImages.clear();
+        mImages.add(BitmapDescriptorFactory.fromResource(R.mipmap.overview));
+        mImages.add(BitmapDescriptorFactory.fromResource(R.mipmap.elit));
 
         // Add a large overlay at Newark on top of the smaller overlay.
-        //mGroundOverlay = depatmentMap.addGroundOverlay(new GroundOverlayOptions()
-//                .image(mImages.get(0))
-//                .position(Shenkar, 800f, 600f));
-//
-//        depatmentMap.setContentDescription("Google Map with ground overlay.");
+        mGroundOverlay = depatmentMap.addGroundOverlay(new GroundOverlayOptions()
+                .image(mImages.get(mCurrentEntry))
+                .position(SHENKAR, 190f, 150f));
 
+        mGroundOverlay2 = depatmentMap.addGroundOverlay(new GroundOverlayOptions()
+                .image(mImages.get(1))
+                .position(ELIT, 190f, 150f));
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            System.out.println("Error");
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            //mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            //mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+            System.out.println("mLastLocation.getLatitude()) ---------> "+ mLastLocation.getLatitude());
+            System.out.println("mLastLocation.getLongitude()) ---------> "+ mLastLocation.getLongitude());
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (depatmentMap != null) {
+            // Access to the location has been granted to the app.
+            depatmentMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "Welcome to ShowShenkar Map", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 }
 
